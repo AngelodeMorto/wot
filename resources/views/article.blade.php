9095da049bfcc0ada99b6c6a30e057bedcb66729
kaspian5 <kaspian5@spaces.ru>
2017-06-10 15:25:49 +0300

    titles style

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,11 +1,18 @@
-@extends('layouts.main');
+@extends('layouts.main')
 
 @section('content')
-
-    <img src="/img/{{ $article->image }}">
-    <h1 class="flex-center article-name">{{ $article->title }}</h1>
-    <p>Опубликовано {{ $article->created_at }} by {{ $article->author }}</p>
-    <div class="row">
-    <div class="col-md-12">{!! $article->text !!}</div>
+    <div class="titles_main">
+        <div class="col-md-12">
+            <div class="thumbnail">
+                <img src="/img/articles/{{ $article->image }}">
+                <div class="caption">
+                    <h1 class="flex-center article-name">{{ $article->title }}</h1>
+                    <p>Опубликовано {{ $article->created_at }} by {{ $article->author }}</p>
+                    <div class="row">
+                        <div class="col-md-12">{!! $article->text !!}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
